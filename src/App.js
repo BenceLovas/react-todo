b67862ea2465f6ff9ae0ab7cddc2b42006e64450
Bence Lovas <bansoul1@gmail.com>
2018-12-27 00:56:17 +0100

    basic todo app implemented with redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/VisibleTodoList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World!
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers/reducers';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const initialState = {
+    visibilityFilter: 'SHOW_ALL',
+    todos: [
+      {
+        id: 0,
+        text: 'Consider using Redux',
+        completed: true
+      },
+      {
+        id: 1,
+        text: 'Keep all state in a single tree',
+        completed: false
+      }
+    ]
+  }
+
+const store = createStore(todoApp, initialState);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
